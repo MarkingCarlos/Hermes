21cc8ef639666ca249698d71c0f7fc7102216f1a
markingcarlos <carlos.a.souza@unesp.br>
2024-08-18 10:43:36 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const texto = "Compromisso com o povo e contra qualquer taxa ou aumento de impostos.";
+    const elemento = document.getElementById("texto");
+    let indice = 0;
+
+    function maquinaEscrever() {
+        if (indice < texto.length) {
+            elemento.innerHTML += texto.charAt(indice);
+            indice++;
+            setTimeout(maquinaEscrever, 100); // Velocidade de digitação (em milissegundos)
+        }
+    }
+
+    maquinaEscrever();
+});
+
+function OpenModal(){
+    document.getElementById('modal').style.display = 'block'
+}
